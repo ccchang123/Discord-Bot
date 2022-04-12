12b77b8493b55f9b58f4b50b67158cb5091a065d
cc_chang <63536886+ccchang123@users.noreply.github.com>
2022-04-13 01:36:04 +0800

    Delete error_code.py

--- error_code.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-
-async def no_permission(no_permission_message):
-    permission_error = await no_permission_message.channel.send('你沒有權限')
-    await asyncio.sleep(1)
-    await permission_error.delete()
-    await no_permission_message.delete()
