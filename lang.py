5357f78575eebff5f7ab4d2cabf9a01b5e75d044
cc_chang <63536886+ccchang123@users.noreply.github.com>
2022-04-13 01:46:32 +0800

    Add files via upload

--- lang.py
+++ lang.py
@@ -7,6 +7,9 @@ def lang_chose(sele):
     if sele == 'zh-tw':
         with open('lang/zh-tw.json', "r", encoding = "utf8") as lang_file:
             language = json.load(lang_file)
+    elif sele == 'zh-cn':
+        with open('lang/zh-cn.json', "r", encoding = "utf8") as lang_file:
+            language = json.load(lang_file)
     elif sele == 'en-us':
         with open('lang/en-us.json', "r", encoding = "utf8") as lang_file:
             language = json.load(lang_file)
