ba308286a18e3c6bbab6aa99acc0e44029cd1003
cc_chang <63536886+ccchang123@users.noreply.github.com>
2022-05-03 13:24:03 +0800

    Add files via upload

---
+++ lib/lang.py
@@ -0,0 +1,40 @@
+import json
+import os
+from lib import self_test
+
+def add_lang():
+    for i in os.listdir('lang'):
+        if i.endswith(".json"):
+            print('find the language file:',i)
+
+async def chlang_check(message, lang, Lang, data):
+    check_config = os.path.isfile('lang/'+lang+'.json')
+    if check_config == False:
+        await message.channel.send(Lang['language-error'])
+        if data['debug-mode'] == 'true':
+            print('load '+lang+' file --- fail')
+        return False
+    else:
+        if data['debug-mode'] == 'true':
+            print('load '+lang+' file --- ok')
+
+def lang_chose(sele, lang_list, data):
+    for i in lang_list:
+        if sele == i:
+            select = 'lang/'+i+'.json'
+            check_config = os.path.isfile(select)
+            if check_config == False:
+                if data['debug-mode'] == 'true':
+                    add_lang()
+                    print('load '+i+' file --- fail',end='\n\n')
+                    self_test.error()
+            else:
+                with open(select, "r", encoding = "utf8") as lang_file:
+                    language = json.load(lang_file)
+                if data['debug-mode'] == 'true':
+                    add_lang()
+                    print('load '+i+' file --- ok',end='\n\n')
+            return language
+    language = None
+    return language
+
---
+++ lib/reset.py
@@ -0,0 +1,85 @@
+config = {
+    "version": "2.6.5",
+    "language": "",
+    "token": "",
+    "command-prefix": "",
+    "custom-status": "online",
+    "custom-activity": "",
+    "owner-id": 0,
+    "local-channel-id": 0,
+    "create-category-id": 0,
+    "picture-only-channel-id": 0,
+    "ticket-category-id": 0,
+    "auto-mute": "",
+    "auto-kick": "",
+    "auto-ban": "",
+
+    "chat-filter": True,
+    "chat-filter-action": "warn",
+
+    "debug-mode": False,
+
+    "commands":{
+        "addadmin": True,
+        "addbypass": True,
+        "ban": True,
+        "clear": True,
+        "chlang": True,
+        "chact": True,
+        "copy": True,
+        "clearwarn": True,
+        "cd": True,
+        "ci": True,
+        "exit": True,
+        "gay": True,
+        "kick": True,
+        "mute": True,
+        "ping": True,
+        "reload": True,
+        "removeadmin": True,
+        "removebypass": True,
+        "showwarn": True,
+        "slowmode": True,
+        "send": True,
+        "time": True,
+        "tlm": True,
+        "tempban": True,
+        "tempmute": True,
+        "unban": True,
+        "unmute": True,
+        "uinfo": True,
+        "warn": True
+    },
+    "command-reset": True,
+
+    "music-bot": True
+}
+
+
+salt = {
+
+}
+
+userdata = {
+    "admin": {
+
+    },
+    "bypass":  {
+
+    },
+    "warns":  {
+
+    }
+}
+
+import json
+
+def reset_config():
+    with open("config.json", "w") as config_file:
+        json.dump(config, config_file, indent = 4)
+    with open("salt.json", "w") as salt_file:
+        json.dump(salt, salt_file, indent = 4)
+    with open("userdata.json", "w") as bypass_file:
+        json.dump(userdata, bypass_file, indent = 4)
+    with open('chatfilter.txt', 'w') as chatfilter_file:
+        chatfilter_file.write('')
